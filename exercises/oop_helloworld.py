0669e0e06b193681b1cce2bbc2faf99f2bb62223
UIH21np <105173655+UIH21np@users.noreply.github.com>
2022-05-09 13:38:07 -0500

    oop_helloworld change

--- exercises/oop_helloworld.py
+++ exercises/oop_helloworld.py
@@ -1,4 +1,15 @@
 # create your class here
+class Car:
+    #instance attributes
+    def __init__(self, Model, Speed):
+        self.model = Model
+        self.speed = Speed 
+
+    #instance method
+    def getModel(self):
+        return self.model
+    def setSpeed(self, newSpeed):
+        self.speed = newSpeed
 
 if __name__ == '__main__':
     bmw = Car('BMW', 30.3)
